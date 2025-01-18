98454638d0257ad33f2230215192b5eb8a457311
Hypercodding <mohammadhammad302@gmail.com>
2025-01-18 13:17:52 +0500

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default ({ mode }) => {
   const proxy_url =
     process.env.VITE_DEV_REMOTE === 'remote'
       ? process.env.VITE_BACKEND_SERVER
-      : 'https://erp-invoice-project.vercel.app/';
+      : 'http://localhost:8888/';
 
   const config = {
     plugins: [react()],
