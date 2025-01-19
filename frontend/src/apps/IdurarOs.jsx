c22f8fdff73daf78fc2106cd222e710915a57b4a
Muhammad Hammad <mohammadhammad302@gmail.com>
2025-01-19 23:36:08 +0500

    Update IdurarOs.jsx

--- frontend/src/apps/IdurarOs.jsx
+++ frontend/src/apps/IdurarOs.jsx
@@ -28,38 +28,38 @@ export default function IdurarOs() {
   );
 
   // // Online state
-  // const [isOnline, setIsOnline] = useState(navigator.onLine);
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
 
-  // useEffect(() => {
-  //   // Update network status
-  //   const handleStatusChange = () => {
-  //     setIsOnline(navigator.onLine);
-  //     if (!isOnline) {
-  //       console.log('🚀 ~ useEffect ~ navigator.onLine:', navigator.onLine);
-  //       notification.config({
-  //         duration: 20,
-  //         maxCount: 1,
-  //       });
-  //       // Code to execute when there is internet connection
-  //       notification.error({
-  //         message: 'No internet connection',
-  //         description: 'Cannot connect to the Internet, Check your internet network',
-  //       });
-  //     }
-  //   };
+  useEffect(() => {
+    // Update network status
+    const handleStatusChange = () => {
+      setIsOnline(navigator.onLine);
+      if (!isOnline) {
+        console.log('🚀 ~ useEffect ~ navigator.onLine:', navigator.onLine);
+        notification.config({
+          duration: 20,
+          maxCount: 1,
+        });
+        // Code to execute when there is internet connection
+        notification.error({
+          message: 'No internet connection',
+          description: 'Cannot connect to the Internet, Check your internet network',
+        });
+      }
+    };
 
-  //   // Listen to the online status
-  //   window.addEventListener('online', handleStatusChange);
+    // Listen to the online status
+    window.addEventListener('online', handleStatusChange);
 
-  //   // Listen to the offline status
-  //   window.addEventListener('offline', handleStatusChange);
+    // Listen to the offline status
+    window.addEventListener('offline', handleStatusChange);
 
-  //   // Specify how to clean up after this effect for performance improvment
-  //   return () => {
-  //     window.removeEventListener('online', handleStatusChange);
-  //     window.removeEventListener('offline', handleStatusChange);
-  //   };
-  // }, [navigator.onLine]);
+    // Specify how to clean up after this effect for performance improvment
+    return () => {
+      window.removeEventListener('online', handleStatusChange);
+      window.removeEventListener('offline', handleStatusChange);
+    };
+  }, [navigator.onLine]);
 
   if (!isLoggedIn)
     return (
